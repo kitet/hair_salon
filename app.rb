a5d3c98105ad44bdd55a2069a6f3f564afa04e6f
kitet <kitetnet@gmail.com>
2017-07-14 17:28:55 +0300

    added two links /owner/stylist/id and /owner/client/id to show individual stylist and client respectively

--- app.rb
+++ app.rb
@@ -5,8 +5,40 @@ require "./lib/client.rb"
 require "./lib/stylist.rb"
 also_reload('lib/**/*.rb')
 
-DB=PG.connect(:dbname=>"hair_salon_test")
+DB=PG.connect(:dbname=>"hair_salon")
 
 get '/' do
 	erb :index
+end
+
+get '/owner' do
+	@stylists=Stylist.all
+	@clients=Client.all
+	erb :owner
+end
+
+post '/owner/stylist/new' do
+	sname=params.fetch('name')
+	stylist=Stylist.new(id:nil,name:sname)
+	stylist.save
+	redirect '/owner'
+end
+
+post '/owner/client/new' do
+	client_name=params.fetch('name')
+	phone=params.fetch('phone')
+	client=Client.new(id: nil,name: client_name, phone: phone)
+	client.save
+	redirect '/owner'
+end
+
+get '/owner/stylist/:id' do
+	@stylist=Stylist.find(params.fetch('id').to_i)
+	@clients=Client.all
+	erb :stylist
+end
+
+get '/owner/client/:id' do
+	@client=Client.find(params.fetch('id').to_i)
+	erb :client
 end
