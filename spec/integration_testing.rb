b14e7bfc20f303c8595860180a459feaa6f8e979
kitet <kitetnet@gmail.com>
2017-07-18 09:59:40 +0300

    added the first integration test/ fails

--- spec/integration_testing.rb
+++ spec/integration_testing.rb
@@ -2,3 +2,11 @@ require 'capybara/rspec'
 require './app'
 Capybara.app=Sinatra::Application
 set(:show_exceptions, false)
+
+describe('owner ',{:type=>:feature}) do
+	it 'home shows button to ' do
+		visit '/'
+		click_link 'Go to Owner Section'
+		expect(page.current_path).to eq '/owner'
+	end
+end
