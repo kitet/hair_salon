5bf63e7e33dbb0b36207d4a9dde111a6de6f1739
kitet <kitetnet@gmail.com>
2017-07-18 19:32:46 +0300

    added test for the update and delete memthods on the class client

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,5 +13,22 @@ RSpec.describe Client do
   it 'all method should return empty at first' do
     expect(Client.all).to eq []
   end
+  #test to check update of name of the client
+  it 'updates the name of the client'  do
+    client1=Client.new(name: 'Patrick Lumumba', id: nil, phone: 747487)
+    client1.save
+    client1.update(name: 'P.L.O Lumumba')
+    expect(client1.cname).to eq 'P.L.O Lumumba'
+  end
+
+  #test to on delete method
+  it 'deletes client from the database' do
+    client1=Client.new(name: 'Patrick Lumumba', id: nil, phone: 747487)
+    client1.save
+    client2=Client.new(name: 'Mambo Mbotela', id: nil, phone: 75757245)
+    client2.save
+    client1.delete
+    expect(Client.all).to eq [client2]
+  end
  end
 end
